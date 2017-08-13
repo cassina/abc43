657e62f6edf28573174a91f1c770e0eb277604b3
Bernardo Cassina Buerba <bernardocassinabuerba@Bernardos-MacBook-Pro.local>
2017-08-13 18:08:38 -0500

    Initial commit.

---
+++ deploy.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+sed -ire 's/#  APP_ENV: development/  APP_ENV: Sandbox/g' app.yaml
---
+++ main.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+import tweepy
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/_b/cron/paseLista')
+def pase_de_lista():
+    pass
+
+
+
+
