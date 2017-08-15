f1402988bfb2944ebd97a52b104b686ca80d9180
Bernardo Cassina Buerba <bernardocassinabuerba@Bernardos-MacBook-Pro.local>
2017-08-14 20:37:12 -0500

    Removed cron and little refactor.

--- app/blueprint.py
+++ app/blueprint.py
@@ -1,24 +1,19 @@
 # -*- coding: utf-8 -*-
 from flask import Blueprint
 
-from .service import format_msg, post_tweet, format_list, pasar_lista
+from .service import format_msg, post_tweet, format_list, pasar_lista, get_time_line
 from .lists import *
 
 bot = Blueprint('bot', __name__)
 
 
+@bot.route('/_b/cron/smoke', methods=['GET'])
+def pasar_lista_43():
+    return 'ok'
+
+
 @bot.route('/_b/cron/pasarLista43', methods=['GET'])
 def pasar_lista_43():
-    # for idx, name in enumerate(normalistas):
-        # count = idx
-        # if idx == 5:
-        #     return 'DONE'
-        # msg = format_msg(hashtags=format_list(hashtags_43),
-        #                  mentions=format_list(responsables),
-        #                  status=status_43,
-        #                  name=name,
-        #                  idx=idx)
-        # post_tweet(msg)
     pasar_lista(lista=normalistas,
                 status=status_43,
                 hashtags=hashtags_43,
@@ -29,17 +24,13 @@ def pasar_lista_43():
 
 @bot.route('/_b/cron/pasarListaABC', methods=['GET'])
 def pasar_lista_abc():
-    # for idx, name in enumerate(normalistas):
-    # count = idx
-    # if idx == 5:
-    #     return 'DONE'
-    # msg = format_msg(hashtags=format_list(hashtags_43),
-    #                  mentions=format_list(responsables),
-    #                  status=status_43,
-    #                  name=name,
-    #                  idx=idx)
-    # post_tweet(msg)
     pasar_lista(lista=abc,
                 status=status_abc,
                 hashtags=hashtags_abc,
                 mentions=responsables)
+
+
+@bot.route('/_b/tweets/delete', methods=['GET'])
+def delete_tweets():
+    timeline = get_time_line()
+    return timeline
